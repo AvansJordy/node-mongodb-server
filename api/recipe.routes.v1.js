3fb8f6bd0e10526b4d95008116d2ba1499d96244
Jcjmfrij <jcjmfrij@avans.nl>
2017-11-24 15:16:23 +0100

    User to Recipe Refactor

--- api/recipe.routes.v1.js
+++ api/recipe.routes.v1.js
@@ -1,5 +1,5 @@
 //
-// ./api/v1/user.routes.v1.js
+// ./api/v1/recipe.routes.v1.js
 //
 var express = require('express');
 var routes = express.Router();
@@ -9,21 +9,21 @@ var User = require('../model/user.model');
 //
 // Geef een lijst van alle users.
 //
-routes.get('/users', function(req, res) {
+routes.get('/recipes', function(req, res) {
     res.contentType('application/json');
     User.find({})
-        .then((users) => {
-            // console.log(users);
-            res.status(200).json(users);
-        })
-        .catch((error) => res.status(401).json(error));
+        .then((recipes) => {
+        // console.log(users);
+        res.status(200).json(recipes);
+})
+    .catch((error) => res.status(401).json(error));
 });
 
 //
 // Retourneer één specifieke users. Hier maken we gebruik van URL parameters.
 // Vorm van de URL: http://hostname:3000/api/v1/users/23
 //
-routes.get('/users/:id', function(req, res) {
+routes.get('/recipes/:id', function(req, res) {
 
 });
 
@@ -31,7 +31,7 @@ routes.get('/users/:id', function(req, res) {
 // Voeg een user toe. De nieuwe info wordt gestuurd via de body van de request message.
 // Vorm van de URL: POST http://hostname:3000/api/v1/users
 //
-routes.post('/users', function(req, res) {
+routes.post('/recipes', function(req, res) {
 
 });
 
@@ -42,7 +42,7 @@ routes.post('/users', function(req, res) {
 // 
 // Vorm van de URL: PUT http://hostname:3000/api/v1/users/23
 //
-routes.put('/users/:id', function(req, res) {
+routes.put('/recipes/:id', function(req, res) {
 
 });
 
@@ -53,7 +53,7 @@ routes.put('/users/:id', function(req, res) {
 // 
 // Vorm van de URL: DELETE http://hostname:3000/api/v1/users/23
 //
-routes.delete('/users/:id', function(req, res) {
+routes.delete('/recipes/:id', function(req, res) {
 
 });
 
--- model/user.model.js
+++ model/user.model.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    name: String
+const RecipeSchema = new Schema({
+    name: String,
+    description: String,
+    imagePath: String
 }, {
     timestamps: true
 });
 
 
-const User = mongoose.model('user', UserSchema);
+const Recipe = mongoose.model('recipe', RecipeSchema);
 
 // Add a 'dummy' user (every time you require this file!)
-const user = new User({
-    name: 'Joe',
-}).save();
+// const user = new User({
+//     name: 'Sloppy Joe',
+//     description: 'its a person or recipe',
+//     imagePath: 'http://del.h-cdn.co/assets/17/25/980x490/landscape-1498148062-delish-homemade-sloppy-joes-horizontal-1024.jpg'
+// }).save();
 
-module.exports = User;
+module.exports = Recipe;
