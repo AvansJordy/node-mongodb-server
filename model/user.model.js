09e6a7a88ed6044822a8fd64d8cdff12bde42893
Jcjmfrij <jcjmfrij@avans.nl>
2017-11-24 15:17:09 +0100

    adada

--- model/user.model.js
+++ model/user.model.js
@@ -12,11 +12,11 @@ const RecipeSchema = new Schema({
 
 const Recipe = mongoose.model('recipe', RecipeSchema);
 
-// Add a 'dummy' user (every time you require this file!)
-// const user = new User({
-//     name: 'Sloppy Joe',
-//     description: 'its a person or recipe',
-//     imagePath: 'http://del.h-cdn.co/assets/17/25/980x490/landscape-1498148062-delish-homemade-sloppy-joes-horizontal-1024.jpg'
-// }).save();
+Add a 'dummy' user (every time you require this file!)
+const user = new User({
+    name: 'Sloppy Joe',
+    description: 'its a person or recipe',
+    imagePath: 'http://del.h-cdn.co/assets/17/25/980x490/landscape-1498148062-delish-homemade-sloppy-joes-horizontal-1024.jpg'
+}).save();
 
 module.exports = Recipe;
